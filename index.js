8b54fc6398332cffd332702f74e450db203de7a7
Hamza Sajid <hamzakhan174722@gmail.com>
2024-07-04 10:37:47 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-
 import express from "express";
+
 const app = express();
-
-const port = 9000; //  here we use virutal port for communication
-
-// Handling a GET Request to the Root URL (/):
-app.get("/", (req, res) => {
+const port = 9000;
+app.use("/", (req, res) => {
   res.json({ message: "Hello From Express App" });
 });
 
-
-//it starts the server and begins listening for incoming HTTP requests on the specified port.
-app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
+app.listen(9000, () => {
+  console.log(`Starting Server on Port ${port}`);
 });
